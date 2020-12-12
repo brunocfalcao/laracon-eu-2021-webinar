aa1afc7c76cf74037c3252a27d24dbc55f0de54e
Bruno Falcao <bruno.c.falcao@brunofalcao.me>
2020-12-12 11:11:00 +0000

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Priority;
+use App\Models\Profile;
+use App\Models\Severity;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,14 +18,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Create Categories
-
         Category::create(['name' => 'Emails']);
         Category::create(['name' => 'Registration']);
         Category::create(['name' => 'Sign In']);
         Category::create(['name' => 'Personal Data']);
         Category::create(['name' => 'Billing']);
 
+        Priority::create(['name' => 'Non-urgent']);
+        Priority::create(['name' => 'Urgent']);
+        Priority::create(['name' => 'Critical']);
+
+        Profile::create(['name' => 'Admin']);
+        Profile::create(['name' => 'Non-Admin']);
+
+        Severity::create(['name' => 'Low']);
+        Severity::create(['name' => 'Urgent']);
+        Severity::create(['name' => 'Critical']);
+        Severity::create(['name' => 'Disruptive']);
 
         User::factory(10)->create()->each(function ($user) {
         });
